b1c3ec2a4050b95227c9663bdfafc95dd0f4015d
Dhairya <32928695+arLong-98@users.noreply.github.com>
2019-12-05 15:28:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """Configurations for telegram_alert.py"""
-bolt_api_key="80a3c426-12de-4101-b92b-327257b28666"
-device_id="BOLT1115928"
+bolt_api_key="xxxxxx"
+device_id="BOLTxxxxx"
 telegram_chat_id="@temp_alert_algo"
-telegram_bot_id="bot1052996860:AAHnPzV6FiA-acrSpK4YtRPTZntXkFh5pTA"
+telegram_bot_id="bot1052996860:xxxxxxx"
 threshold=250
